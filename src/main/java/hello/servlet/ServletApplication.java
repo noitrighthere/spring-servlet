0153a0c68a9066f6f47a1341f66fa97c9da5d646
nokh9503 <nokh9503@hanmail.net>
2022-03-16 23:14:35 +0900

    [추가] 스프링 MVC - 구조 이해

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -1,8 +1,10 @@
 package hello.servlet;
 
+import hello.servlet.web.springmvc.v1.SpringMemberForomControllerV1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 
 @ServletComponentScan	// 서블릿 자동 등록
 @SpringBootApplication
@@ -11,4 +13,12 @@ public class ServletApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
+
+	// SpringMemberForomControllerV1 스프링 빈 직접 등록
+	/*
+	@Bean
+	SpringMemberForomControllerV1 springMemberForomControllerV1() {
+		return new SpringMemberForomControllerV1();
+	}
+	 */
 }
